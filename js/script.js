0945bafb9f679ee4f0d8fc7de2947f548aaf0eab
Rodney Gakuru <gakururodney@gmail.com>
2020-08-12 00:37:20 -0400

    modeled the project

--- js/script.js
+++ js/script.js
@@ -1,38 +1,164 @@
-var num = parseInt(document.getElementById("undo").value);
-var totalAmount = num*(tas + s + c + top);
-alert(" The Total Amount is kshs "  +  totalAmount  +  ""  +  " Thank you for your order welcome again.");
-prompt("Enter your name")
-prompt("enter your location")
-alert("You delivery fee is 150/=")
-alert("You order will be delivered shortly")
-function Pizza (size, toppings) {
-    this.pizzaSize = size;
-    this.pizzaToppings = toppings;
-  }
+function pizza(name) {
+  this.pizzaName = name;
+}
+pizza.prototype.nameSize = function() {
+  if (this.pizzaName === "kindergafring.") {
+      return "kindergafring.";
+  }else if(this.pizzaName === "Burger.") {
+      return "Burger.";
+  }else if(this.pizzaName === "Deluxe.") {
+      return "Deluxe.";
+  }else if(this.pizzaName === "Macon BBQ.") {
+      return "Macon BBQ.";
+  }else if(this.pizzaName === "Beef Pepperoni.") {
+      return "Beef Pepperoni.";
+  }else if(this.pizzaName === "litle Feast.") {
+      return "litle Feast.";
+  }else if(this.pizzaName === "qwent Feta.") {
+      return "Roast Veg & Feta.";
+  }else if(this.pizzaName === "Spongy Boerewors.") {
+      return "Spongy Boerewors.";
+  }else {
+      alert("Please select a pizza type to continue");
+  };
+}
 
-  Pizza.prototype.costOfPizza = function (size, toppings) {
-    var pizzaPrice = this.pizzaSize + this.toppings;
-    return pizzaPrice;
+function size(name) {
+  this.sizeName = name;
+}
+
+size.prototype.priceSize = function() {
+  if (this.sizeName === "large") {
+      // alert("Large");
+      return 4500;
+  } else if(this.sizeName === "medium") {
+      // alert("Not large");
+      return 3150;
+  } else if(this.sizeName === "small") {
+      return 1900;
+  } else {
+      alert("Please select a pizza size");
   }
-  $(document).ready(function(){
-    $("#new-pizza-order").submit(function(event){
+}
+
+function crust(name) {
+  this.crustName = name;
+}
+
+crust.prototype.crustPrice = function() {
+  if (this.crustName === "cheese") {
+      // alert("cheese");
+      return 100;
+  } else if (this.crustName === "thin") {
+      return 70;
+  } else if (this.crustName === "neapolitan") {
+      return 120;
+  } else {
+      alert("Please select a prefferable crust to continue");
+  }
+}
+
+function topping(name) {
+  this.toppingName = name;
+}
+
+topping.prototype.toppingPrice = function() {
+  if (this.toppingName === "Pepperoni") {
+      return 10;
+  } else if (this.toppingName === "mushrooms") {
+      return 40;
+  } else if (this.toppingName === "Onions") {
+      return 15;
+  } else if (this.toppingName === "Sausage") {
+      return 20;
+  } else if (this.toppingName === "Bacon") {
+      return 30;
+  } else if (this.toppingName === "Extra-cheese") {
+      return 10;
+  } else if (this.toppingName === "Black-olives") {
+      return 15;
+  } else if (this.toppingName === "Green-peppers") {
+      return 5;
+  } else {
+      alert("Please choose topping(s) of your choice");
+  }
+}
+
+
+
+function quantity(name) {
+  this.quantityName = name;
+}
+
+quantity.prototype.quantityPrice = function() {
+
+}
+
+$(document).ready(function() {
+  $("#delivery").click(function() {
+      alert("For to door deliveries. Please confirm by pressing ok to continue");
+      prompt("Please Enter your mobile number");
+      prompt("Enter your current location");
+  });
+});
+
+
+$(document).ready(function() {
+  $("#checkout").click(function() {
       event.preventDefault();
 
-      var inputtedPizzaSize = parseInt( $("select.new-pizza-size").val() );
-      var inputtedPizzaToppings = 0;
+  var pizzaInput = $("#type").val();
 
-      var checkedBoxes = document.getElementsByName("toppings");
-      for (i = 0; i < checkedBoxes.length; i += 1) {
-        if (checkedBoxes[i].checked) {
-          inputtedPizzaToppings += 1;
-        }
-      }
-      var newPizza = new Pizza(inputtedPizzaSize, inputtedPizzaToppings);
+      pizzaSelect = new pizza(pizzaInput);
+      pizzaSelect.nameSize();
+      // alert(pizzaSelect.nameSize());
 
-      $("h1#pizza-price").text("$" + newPizza.costOfPizza());
+  var sizeInput = $("#size").val();
 
-      console.log(inputtedPizzaSize);
-      console.log(inputtedPizzaToppings);
-      console.log(newPizza);
-    });
+  var pizzaSize = new size(sizeInput);
+      pizzaSize.priceSize();
+      // alert(pizzaSize.priceSize());
+
+  var crustInput = $("#crust").val();
+  
+  var crustSize = new crust(crustInput);
+      crustSize.crustPrice();
+      // alert(crustSize.crustPrice());
+
+
+  var toppingInput = $("#toppings").val();
+
+  var toppingSize = new topping(toppingInput);
+      toppingSize.toppingPrice();
+      // alert(toppingSize.toppingPrice());
+
+
+  var quantityInput = $("quantity").val();
+
+  var  quantitySize = new quantity (quantityInput);
+       quantitySize.quantityPrice();
+      //  alert(quantitySize.quantityPrice());
+
+ // calcTotal to calculate the total pizza summation
+
+  var pizzaPrice = (parseInt(parseInt(pizzaSize.priceSize()) + parseInt(crustSize.crustPrice()) + parseInt(toppingSize.toppingPrice())));
+  $("#summation").append(pizzaPrice);
+
+  // S-size, C-crust, T-toppings N-name
+
+  var pizzaS = (pizzaSize.priceSize());
+  $("#psize").append(pizzaS);
+
+  var pizzaC = (crustSize.crustPrice());
+  $("#cprice").append(pizzaC);
+
+  var pizzaT = (toppingSize.toppingPrice());
+  $("#tprice").append(pizzaT);
+
+  var pizzaN = (pizzaSelect.nameSize());
+  $("#ptype").append(pizzaN);
+  event.preventDefault();
+  
   });
+})
+
---
+++ read.md
@@ -0,0 +1,15 @@
+# project name 
+ pizza inn 
+# Project Description
+This is a project based on the website that orders pizza and are delivered and payments on delivery
+# Authours 
+*Name;Rodney Gakuru
+# Setup instructions
+it is based on use of javascript,css and html 
+# Technologies used
+* HTML
+* javascript
+* CSS
+## LICENSE and Copyright
+licence and copyright  mit lincence MIT License
+
